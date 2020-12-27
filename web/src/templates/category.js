4ab4226e6ae42a046dc420680783fa070f8d22f1
Nico <1916148+nrmarston@users.noreply.github.com>
2020-12-27 00:10:29 -0600

    fixed the categories pages

--- web/src/templates/category.js
+++ web/src/templates/category.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import {graphql} from 'gatsby'
+import {graphql, Link} from 'gatsby'
 import Container from '../components/container'
 import GraphQLErrorList from '../components/graphql-error-list'
 import SEO from '../components/seo'
 import Layout from '../containers/layout'
+// Import a function to build the blog URL
+import {getBlogUrl} from '../lib/helpers'
 
+// Add “posts” to the GraphQL query
 export const query = graphql`
   query CategoryTemplateQuery($id: String!) {
     category: sanityCategory(id: {eq: $id}) {
       title
       description
+      posts {
+        _id
+        title
+        publishedAt
+        slug {
+          current
+        }
+      }
     }
   }
 `
 const CategoryPostTemplate = props => {
   const {data = {}, errors} = props
-  const {title, description} = data.category || {}
+  // Destructure the new posts property from props
+  const {title, description, posts} = data.category || {}
 
   return (
     <Layout>
@@ -26,6 +38,22 @@ const CategoryPostTemplate = props => {
         <article>
           <h1>Category: {title}</h1>
           <p>{description}</p>
+          {/*
+            If there are any posts, add the heading,
+            with the list of links to the posts
+          */}
+          {posts && (
+            <React.Fragment>
+              <h2>Posts</h2>
+              <ul>
+                { posts.map(post => (
+                  <li key={post._id}>
+                    <Link to={getBlogUrl(post.publishedAt, post.slug)}>{post.title}</Link>
+                  </li>))
+                }
+              </ul>
+            </React.Fragment>)
+          }
         </article>
       </Container>
     </Layout>
